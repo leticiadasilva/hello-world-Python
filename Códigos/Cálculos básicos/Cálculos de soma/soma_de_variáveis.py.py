9fc0f499b516573cdd733abd28de950d6536a213
Letícia Silva <leticiadasilva.contato@gmail.com>
2020-01-10 14:37:17 -0300

    Update soma_de_variáveis.py.py

--- Códigos/Cálculos básicos/Cálculos de soma/soma_de_variáveis.py.py
+++ Códigos/Cálculos básicos/Cálculos de soma/soma_de_variáveis.py.py
@@ -1,6 +1,12 @@
+# Soma de dois valores inteiros
+
+# Declare o primeiro valor
 primeiro_valor = 10
+# Declare o segundo valor
 segundo_valor = 9
 
+# Crie uma variável para guardar o cálculo de somatória
 soma = primeiro_valor + segundo_valor
 
-print ("X = ", soma)
+# Imprima o resultado
+print ("SOMA = ", soma)
