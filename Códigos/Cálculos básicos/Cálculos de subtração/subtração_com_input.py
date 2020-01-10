a97ccd5676c49e3f630b64c120f7b6aa818ae094
Letícia Silva <leticiadasilva.contato@gmail.com>
2020-01-10 10:42:58 -0300

    Update subtração_com_input.py

--- Códigos/Cálculos básicos/Cálculos de subtração/subtração_com_input.py
+++ Códigos/Cálculos básicos/Cálculos de subtração/subtração_com_input.py
@@ -1,7 +1,7 @@
 primeiro_valor = input("Digite o primeiro valor: ")
 segundo_valor = input("Digite o segundo valor: ")
 
-subtração = int(primeiro_valor)-int(segundo_valor)
+subtração = int(primeiro_valor) - int(segundo_valor)
 
 print ("SUB = ", str(subtração))
 
