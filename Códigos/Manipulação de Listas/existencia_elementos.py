6ee3db159c1fc20dafb3d40d827ff8907b181fb8
Ramon Brandi <ramonbrand@gmail.com>
2020-10-12 14:41:19 -0300

    Add-Manipulacao-Lista

---
+++ Códigos/Manipulação de Listas/existencia_elementos.py
@@ -0,0 +1,7 @@
+lista_string = ['peixe', 'ovo', 'tomate', 'bacon']
+
+# utilizando in retorna um valor booleano.
+contem_na_lista = 'ovo' in lista_string
+
+#  Elemento existe na lista = true / elemento n]ao existe = False
+print(contem_na_lista)
